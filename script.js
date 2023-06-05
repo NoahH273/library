f82a96b4fbce18f8a45bbf818acdb1d311191dc5
NoahH273 <nhymson9@icloud.com>
2023-06-05 15:21:41 -0700

    Style Popup Form

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ addBookButton.addEventListener('click', formClick)
 function formClick () {
     const overlay = document.querySelector(".overlay");
     overlay.style.visibility = "visible";
-    const form = document.querySelector('.popup');
-    form.style.visibility = "visible";
 }
