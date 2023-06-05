94124ef9998b4d92e3f8b709f7af819eae0f31b0
NoahH273 <nhymson9@icloud.com>
2023-06-05 15:51:29 -0700

    Add Book Object Creation on Form Completion

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const addBookButton = document.querySelector(".add-book");
-addBookButton.addEventListener('click', formClick)
+addBookButton.addEventListener('click', formOpen)
+const closeFormButton = document.querySelector(".close-button");
+closeFormButton.addEventListener('click', formClose);
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', addBook)
 
-function formClick () {
+function formOpen () {
     const overlay = document.querySelector(".overlay");
     overlay.style.visibility = "visible";
+}
+
+function formClose () {
+    const overlay = document.querySelector(".overlay");
+    overlay.style.visibility = "hidden";
+}
+
+function addBook () {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+    const newBook = new Book(title, author, pages, read);
+}
+
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
