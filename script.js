389af4c11a61ca7889c413cb5c75ebacdc7eaaa7
NoahH273 <nhymson9@icloud.com>
2023-06-19 17:53:00 -0700

    Finish Project

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const overlay = document.querySelector(".overlay");
 overlay.addEventListener('click', formClose);
 const popup = document.querySelector(".create-book");
 popup.addEventListener('click', (e) => e.stopPropagation());
+const addSuggestedButtons = document.querySelectorAll(".add-suggested");
+addSuggestedButtons.forEach(button => button.addEventListener('click', addSuggested))
 let books = [];
 
 
@@ -24,15 +26,22 @@ function formClose (e) {
 }
 
 function addBook (e) {
-    e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").checked;
-    const newBook = makeBook(title, author, pages, read);
-    const content = document.querySelector(".content");
-    content.appendChild(newBook);
-    formClose();
+    const form = document.querySelector(".create-book");
+    if(!form.checkValidity()) {
+        return;
+    }
+    else {
+        e.preventDefault();
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("read").checked;
+        const newBook = makeBook(title, author, pages, read);
+        const content = document.querySelector(".content");
+        content.appendChild(newBook);
+        formClose();
+    }
+    
 }
 
 function Book (title, author, pages, read) {
@@ -43,35 +52,52 @@ function Book (title, author, pages, read) {
 }
 
 function makeBook (title, author, pages, read) {
+
+    //Creating book object
     const newBook = new Book(title, author, pages, read);
     books.push(newBook);
+
+    //creating DOM elements
     const container = document.createElement("article");
-    container.classList.add("book");
     const header = document.createElement("h3");
-    header.classList.add("book-title");
-    header.textContent = title;
     const info = document.createElement("dl");
     const authorLabel = document.createElement("dt");
-    authorLabel.textContent = "Author:";
     const authorInfo = document.createElement("dd");
-    authorInfo.textContent = author;
     const pagesLabel = document.createElement("dt");
-    pagesLabel.textContent = "Pages:";
     const pagesInfo = document.createElement("dd");
-    pagesInfo.textContent = pages;
     const readLabel = document.createElement("dt");
-    readLabel.textContent = "Read:";
     const readInfo = document.createElement("dd");
     const readCheckbox = document.createElement("input");
+
+    //setting classes/attributes
+    container.classList.add("book");
+    header.classList.add("book-title");
+    header.textContent = title;
+    authorLabel.textContent = "Author:";
+    authorInfo.textContent = author;
+    pagesLabel.textContent = "Pages:";
+    pagesInfo.textContent = pages;
+    readLabel.textContent = "Read:";
     readCheckbox.classList.add("read-button");
     readCheckbox.type = "checkbox";
     readCheckbox.name = "read-button";
+    if(read === true) readCheckbox.checked = true;
+
+    //appending elements
     readInfo.appendChild(readCheckbox);
     info.append(authorLabel, authorInfo, pagesLabel, pagesInfo, readLabel, readInfo);
     container.append(header, info);
+
+    //adding event listeners
+
     return container;
+}
 
-
-
-
+function addSuggested () {
+    //"this" is the button to add book
+    const book = this.parentNode.parentNode;
+    book.remove();
+    const userLibrary = document.querySelector(".user.content");
+    userLibrary.appendChild(book);
+    this.remove();
 }
